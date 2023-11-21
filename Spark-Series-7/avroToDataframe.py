cd6c81eda70ae089d3f14220b2061301a67c3ffb
arunadas <aruna.das29@gmail.com>
2023-11-21 14:28:30 -0800

    adding modern file processing

---
+++ Spark-Series-7/avroToDataframe.py
@@ -0,0 +1,34 @@
+"""
+ Avro ingestion in a dataframe.
+
+ Source of file: util directory
+ I created the avro file using spark from csv
+
+ @author Aruna Das
+"""
+from pyspark.sql import SparkSession
+import os
+import logging
+
+current_dir = os.path.dirname(__file__)
+relative_path = "../data/street_Names.avro"
+absolute_file_path = os.path.join(current_dir, relative_path)
+
+# Creates a session on a local master
+spark = SparkSession.builder.appName("avro to Dataframe") \
+    .master("local[*]").getOrCreate()
+
+# set the log level to ERROR it will remove the clutter from the console
+spark.sparkContext.setLogLevel("ERROR")
+
+
+# Reads a Parquet file, stores it in a dataframe
+df = spark.read.format("avro") \
+    .load(absolute_file_path)
+
+# Shows at most 10 rows from the dataframe
+df.show(10)
+df.printSchema()
+print("The dataframe has {} rows.".format(df.count()))
+
+spark.stop()
