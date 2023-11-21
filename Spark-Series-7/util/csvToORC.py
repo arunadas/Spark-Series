5201ea73598dafab19a34bce941d932d28a36288
arunadas <aruna.das29@gmail.com>
2023-11-21 14:27:24 -0800

    adding modern file processing

---
+++ Spark-Series-7/util/csvToORC.py
@@ -0,0 +1,47 @@
+"""
+ CSV ingestion in a dataframe.
+ Source of file: https://data.gov/
+ @author Aruna Das
+"""
+from pyspark.sql import SparkSession
+import os
+import logging
+
+current_dir = os.path.dirname(__file__)
+relative_path = "../data/Street_Names.csv"
+absolute_file_path = os.path.join(current_dir, relative_path)
+
+# Creates a session on a local master
+spark = SparkSession.builder.appName("CSV to Dataframe") \
+    .master("local[*]").getOrCreate()
+
+# set the log level to ERROR it will remove the clutter from the console
+spark.sparkContext.setLogLevel("ERROR")
+
+print("Using Apache Spark v{}".format(spark.version))
+
+# Reads a CSV file with header, called Street_Names.csv, stores it in a
+# dataframe
+df = spark.read.format("csv") \
+        .option("header", "true") \
+        .option("multiline", True) \
+        .option("sep", ",") \
+        .option("inferSchema", True) \
+        .load(absolute_file_path)
+
+print("Show dataframe content:")
+
+# Shows at most 10 rows from the dataframe, with columns as wide as 20
+# characters
+df.show(10, 20)
+print("Dataframe's schema:")
+df.printSchema()
+
+#orc_file_name = 
+orc_file_path = os.path.join(current_dir,"../data/Street_Names.orc")
+
+df.write.format("orc")\
+          .mode("overwrite")\
+          .save(orc_file_path)
+
+spark.stop()
